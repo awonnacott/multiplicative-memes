b924ee88bef9ad3f2ef8ad637e9484e9f8806335
Andrew Wonnacott <awonnacott@princeton.edu>
2018-01-02 01:46:29 -0500

    espn

---
+++ espn.py
@@ -0,0 +1,50 @@
+
+
+#!/usr/bin/env python3
+
+from bs4 import BeautifulSoup
+import requests
+import csv
+import os
+
+
+positions = {
+    'QB': 0,
+    'RB': 2,
+    'WR': 4,
+    'TE': 6,
+    'DST': 16,
+    'K': 17,
+    'FLEX': 23
+}
+weeks = range(1, 22)
+
+
+def cbs(pos, week):
+    url = 'http://games.espn.com/ffl/tools/projections?&scoringPeriodId={week}&seasonId=2017&slotCategoryId={pos}'.format(pos=positions[pos], week=week)
+    try:
+        contents = requests.get(url).content
+        soup = BeautifulSoup(contents, "html.parser")
+        rows = soup.find('table').find_all('tr')
+        header = [td.text.split(',')[0].replace(u'\xa0', u'') for td in rows[2]]
+        data = [[td.text.split(',')[0].split('D/ST')[0] for td in row] for row in rows[3:]]
+    except AttributeError:
+        print("Failed: " + pos + " " + str(week))
+        return
+    filename = 'espn_{pos}_week{week}.csv'.format(pos=pos, week=week)
+    with open(filename, 'w') as f:
+        print('writing: ', filename)
+        writer = csv.writer(f)
+        try:
+            writer.writerow(header)
+            writer.writerows(data)
+        except UnicodeEncodeError:
+            print("Failed: " + pos + " " + str(week))
+            print(header)
+            print(data)
+            return
+
+
+os.chdir('espn')
+[cbs(pos, week) for pos in positions for week in weeks]
+os.chdir('..')
