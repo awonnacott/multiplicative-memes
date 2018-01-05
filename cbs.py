ca76fe382431fbfc2360d42e50495910d9591c82
eneyman <34433414+eneyman@users.noreply.github.com>
2018-01-05 01:24:27 -0500

    Update cbs.py

--- cbs.py
+++ cbs.py
@@ -7,19 +7,15 @@ import os
 
 positions = ['QB', 'RB', 'WR', 'TE', 'K', 'DST']
 weeks = range(1, 18)
-writers = ['jamey_eisenberg', 'dave_richard']
-source = ['cbs1', 'cbs2']
-leaguetype = 'standard'
-
 
 def cbs(pos, week, leaguetype, writer):
-    url = 'https://fantasynews.cbssports.com/fantasyfootball/stats/weeklyprojections/{pos}/{week}/{writer}/{leaguetype}?&print_rows=9999'.format(pos=pos, week=week, leaguetype=leaguetype, writer=writer)
+    url = 'https://fantasynews.cbssports.com/fantasyfootball/stats/weeklyprojections/{pos}/{week}/avg/standard?&print_rows=9999'.format(pos=pos, week=week, leaguetype=leaguetype, writer=writer)
     contents = requests.get(url).content
     soup = BeautifulSoup(contents, "html.parser")
     rows = soup.find('table').find_all('tr')
     header = [td.text.split(',')[0] for td in rows[2]]
     data = [[td.text.split(',')[0] for td in row] for row in rows[3:-1]]
-    filename = 'week{week}_{pos}_cbs_{writer}.csv'.format(pos=pos, week=week, writer=writer)
+    filename = 'cbs_{pos}_week{week}.csv'.format(pos=pos, week=week)
     with open(filename, 'w') as f:
         print('writing: ', filename)
         writer = csv.writer(f)
@@ -28,5 +24,5 @@ def cbs(pos, week, leaguetype, writer):
 
 
 os.chdir('cbs')
-[cbs(pos, week, leaguetype, writer) for pos in positions for week in weeks for writer in writers]
+[cbs(pos, week, leaguetype) for pos in positions for week in weeks]
 os.chdir('..')
