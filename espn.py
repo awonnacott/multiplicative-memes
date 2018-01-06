72b6f2bd825f1363270c62434e24e66b6aecb139
Andrew Wonnacott <awonnacott@princeton.edu>
2018-01-06 16:40:48 -0500

    espn all the pages

--- espn.py
+++ espn.py
@@ -1,12 +1,9 @@
-
-
 #!/usr/bin/env python3
 
 from bs4 import BeautifulSoup
 import requests
 import csv
 import os
-
 
 positions = {
     'QB': 0,
@@ -21,13 +18,21 @@ weeks = range(1, 22)
 
 
 def cbs(pos, week):
-    url = 'http://games.espn.com/ffl/tools/projections?&scoringPeriodId={week}&seasonId=2017&slotCategoryId={pos}'.format(pos=positions[pos], week=week)
+    url = 'http://games.espn.com/ffl/tools/projections?&scoringPeriodId={week}&seasonId=2017&slotCategoryId={pos}&startIndex={i}'.format(pos=positions[pos], week=week, i="{i}")
     try:
-        contents = requests.get(url).content
+        contents = requests.get(url.format(i=0)).content
         soup = BeautifulSoup(contents, "html.parser")
         rows = soup.find('table').find_all('tr')
         header = [td.text.split(',')[0].replace(u'\xa0', u'') for td in rows[2]]
         data = [[td.text.split(',')[0].split('D/ST')[0] for td in row] for row in rows[3:]]
+        contents = requests.get(url.format(i=50)).content
+        soup = BeautifulSoup(contents, "html.parser")
+        rows = soup.find('table').find_all('tr')
+        data += [[td.text.split(',')[0].split('D/ST')[0] for td in row] for row in rows[3:]]
+        contents = requests.get(url.format(i=100)).content
+        soup = BeautifulSoup(contents, "html.parser")
+        rows = soup.find('table').find_all('tr')
+        data += [[td.text.split(',')[0].split('D/ST')[0] for td in row] for row in rows[3:]]
     except AttributeError:
         print("Failed: " + pos + " " + str(week))
         return
